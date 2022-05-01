9cd5167d0b90215d6876293ff5f9e46304d1a599
Arya Danech <aryadanech9@gmail.com>
2022-05-01 17:10:10 +0530

    Storing Data into Database using Models

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,15 @@
 const express =  require('express');
+const User = require('../models/User');
 const router = express.Router();
 
-router.get('/', (req,res)=>{
-    obj = {
-        a: 'hello',
-        b: 'world'
-    }
-    res.json(obj)
+
+
+//Creating a User using: POST "/api/auth"
+router.post('/', (req,res)=>{
+    console.log(req.body);
+    const user = User(req.body);
+    user.save();
+    res.send("Hello!!!");
 })
 
 module.exports = router;
